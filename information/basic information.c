0e511a7931ff6cc3e544dbc001bda121b8bfa3c0
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2023-09-03 06:47:03 +0530

    Add files via upload
    3/09/2023

---
+++ information/basic information.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{ 
+    int a=16,b=90797,c=72485,e=91,f=141,g=22,h=9,i=2007;
+    char d='o';
+
+	printf("name:jaydeep\n");
+	printf("age:%d\n",a);
+	printf("Dob:%d/%d/%d\n",g,h,i);
+    printf("contact.no:+%d%d %d\n",e,b,c);
+    printf("blood grop:%c\n",d);
+    printf("add:%d,suncity row house dindoli,surat\n",f);
+    printf("hobby:gaming,swimming\n");
+    printf("lenguage known:english,hindi,gujrati\n");
+    printf("last school name:gyanmanajari\n");
+    printf("living city:surat\n");
+    printf("i am bilong for:rajsthan\n");
+    printf("i am currently study in: rnw\n");
+    printf("I'm currently learning programing languages like c,c++,java and dart\n");
+    printf("i like travelling\n");
+    printf("most like food:momos");
+}
