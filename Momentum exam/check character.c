6ea105ffdae11780b2e4f494d7691d56b78faae5
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2023-09-14 18:07:48 +0530

    Update check character.c

--- Momentum exam/check character.c
+++ Momentum exam/check character.c
@@ -3,23 +3,19 @@
 main()
 {
 	char a;
-	printf("enter any value:");
+	printf("enter any character:");
 	scanf("%c",&a);
 	
-	if(a>=65 && a<=90)
+	if((a>=97 && a<=122) || (a>=65 && a<=90))
 	{
-		printf("%c is capital alphabet",a);
+		printf("%c is alphabet",a);
 	}
-	else if(a>=97 && a<=122)
+	else if(a>=48 && a<=57)
 	{
-		printf("%c is small alphabet",a);
-	}
-	else if(a<=48 && a<=57)
-	{
-	    printf("%d in numder");	
+	    printf("%c is number",a);	
 	}
 	else
 	{
-		printf("in special char");
+		printf("%c is special character",a);
 	}
 }
