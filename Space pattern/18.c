f9ac61a8e8b6e55b64aee485f20028f2b369e1e0
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2023-09-26 23:38:22 +0530

    Add files via upload

---
+++ Space pattern/18.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+	int a, b, c;
+	for(a=1; a<=5; a++)
+	{
+		for(c=a; c<5; c++)
+		{
+			printf(" ");
+		}
+		for(b=1; b<=a; b++)
+		{
+			printf("%d",a%2);
+		}
+		printf("\n");
+	}
+}
