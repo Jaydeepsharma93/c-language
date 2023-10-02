4b7f50e1abafa507b92b17e64f683abfe5349e88
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2023-10-02 23:06:21 +0530

    Add files via upload

---
+++ array/array 1 to n.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+	int n,i;
+	printf("enter tha size of array:",n);
+	scanf("%d",&n);
+	int a[n];
+	for(i=0; i<n; i++)
+	{
+	  printf("a[%d]:",i);
+	  scanf("%d",&a[i]);	
+	}
+	for(i=0; i<n; i++)
+	{
+	  printf("a[%d]:%d  ",i,a[i]);
+	}
+}
