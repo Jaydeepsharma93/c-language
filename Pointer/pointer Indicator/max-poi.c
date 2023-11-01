ea04498fe7c22d5a501fc85317a6914474bc4e33
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2023-11-01 18:31:11 +0530

    Update max-poi.c

--- Pointer/pointer Indicator/max-poi.c
+++ Pointer/pointer Indicator/max-poi.c
@@ -2,27 +2,6 @@
  
 main()
 {
-  int *maximum,n, i, location = 1;
- 
-  printf("Enter the size of array : ");
-  scanf("%d", &n);
-  int arr[n];
- 
-  for (i=0; i<n ; i++)
-  {
-  	printf("array[%d] : ",i);
-    scanf("%d", &arr[i]);
-  }
-   maximum  = arr;
-  *maximum = *arr;
- 
-  for (i=1; i<n; i++)
-  {
-    if(*(arr+i) > *maximum)
-    {
-       *maximum = *(arr+i);
-    }
-  }
- 
-  printf("Maximum num in array is: %d",*maximum);
+   printf("khusi");
+
 }
